e64b12d003976c58265fbffc04da37dff17628cb
CsTarepanda <sankaku900622@yahoo.co.jp>
2016-05-15 03:13:30 +0900

    file受け取りから保存

--- missphoto/postUpload.py
+++ missphoto/postUpload.py
@@ -1,18 +1,22 @@
 from django.shortcuts import redirect
+from django.core.urlresolvers import reverse
+
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
 
 
 def index(request):
-    if request.method == "post":
+    if request.method == "POST":
         return post(request)
     else:
         return get(request)
 
 
 def post(request):
-    for i in request.FILES:
-        print(i)
-    return redirect("upload")
+    for name, value in request.FILES.items():
+        default_storage.save("missphoto/static/photos/{}".format(value), ContentFile(value.read()))
+    return redirect(reverse("upload"))
 
 
 def get(request):
-    return redirect("")
+    return redirect(reverse("index"))
