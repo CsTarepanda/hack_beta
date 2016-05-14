b816a42ce88114410a36f52365feb21e9a5c85e7
sinshutukibotu <c0114292a2@edu.teu.ac.jp>
2016-05-15 02:38:13 +0900

    モデルの修正

--- missphoto/admin.py
+++ missphoto/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 # Register your models here.
 
-from .models import Post, Good, Bad, PhotosData
+from .models import Post, Good, Bad, PhotosData, Tag
 
 admin.site.register(Post)
 admin.site.register(Good)
 admin.site.register(Bad)
 admin.site.register(PhotosData)
+admin.site.register(Tag)
--- missphoto/models.py
+++ missphoto/models.py
@@ -5,21 +5,41 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model):
-    thema = models.CharField(max_length=200)
     user_id = models.ForeignKey(User)
     created = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return str(self.created)
 
 
 class Good(models.Model):
     post_id = models.ForeignKey(Post)
     user_id = models.ForeignKey(User)
 
+    def __str__(self):
+        return str(self.user_id) + ":" + str(self.post_id)
+
 
 class Bad(models.Model):
     post_id = models.ForeignKey(Post)
     user_id = models.ForeignKey(User)
 
+    def __str__(self):
+        return str(self.user_id) + ":" + str(self.post_id)
+
 
 class PhotosData(models.Model):
     post_id = models.ForeignKey(Post)
-    data = models.TextField()
+    name = models.CharField(max_length=200, default='default')
+    created = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    post_id = models.ForeignKey(Post)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
