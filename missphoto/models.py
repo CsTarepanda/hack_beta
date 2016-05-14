c4d13407222d3add2e32ffaa7ce29d70b6413b2b
CsTarepanda <sankaku900622@yahoo.co.jp>
2016-05-14 16:37:17 +0900

    add model class 中身無し

--- missphoto/models.py
+++ missphoto/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Photos(models.Model):
+    pass
+
+
+class Goods(models.Model):
+    pass
+
+
+class Bads(models.Model):
+    pass
