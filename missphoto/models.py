d9c1139bd9755ffc03f8aa3e2cd6a3e5563ca065
CsTarepanda <sankaku900622@yahoo.co.jp>
2016-05-14 19:09:06 +0900

    add model 中身

--- missphoto/models.py
+++ missphoto/models.py
@@ -1,15 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
 
-class Photos(models.Model):
+class CustomUser(AbstractUser):
     pass
+
+
+class Posts(models.Model):
+    thema = models.CharField(max_length=200)
+    user_id = models.ForeignKey(CustomUser)
+    created = models.DateField(auto_now=True)
 
 
 class Goods(models.Model):
-    pass
+    post_id = models.ForeignKey(Posts)
+    user_id = models.ForeignKey(CustomUser)
 
 
 class Bads(models.Model):
-    pass
+    post_id = models.ForeignKey(Posts)
+    user_id = models.ForeignKey(CustomUser)
