1af6323f98ca1f62a382022efa092df2c66b8cce
CsTarepanda <sankaku900622@yahoo.co.jp>
2016-05-15 08:50:23 +0900

    api完成　タグ取得

--- missphoto/postUpload.py
+++ missphoto/postUpload.py
@@ -4,7 +4,11 @@ from django.core.urlresolvers import reverse
 from django.core.files.storage import default_storage
 from django.core.files.base import ContentFile
 
+from missphoto.models import *
 from time import time
+
+import json
+import subprocess
 
 
 def index(request):
@@ -16,7 +20,27 @@ def index(request):
 
 def post(request):
     for name, value in request.FILES.items():
-        default_storage.save("missphoto/static/photos/{}{}.{}".format(request.user, int(time()), str(value).split(".")[-1]), ContentFile(value.read()))
+        filename = "missphoto/static/photos/{}{}.{}".format(request.user, int(time()), str(value).split(".")[-1])
+        default_storage.save(filename, ContentFile(value.read()))
+        post = Post(user_id=request.user)
+        post.save()
+        # - - - 無理やり cloud vision
+        subprocess.call("php missphoto/gcv.php {} > missphoto/gcv_result".format(filename), shell=True)
+        re = open("missphoto/gcv_result")
+        re.readline()
+        result = "{"
+        for i in re:
+            if i.startswith("<"):
+                break
+            result += i
+
+        result = json.loads(result)
+        result = result.get("responses")[0].get("labelAnnotations")
+        result = [x.get("description") for x in result]
+        # - - -
+        for i in result:
+            Tag(post_id=post, name=i).save()
+        PhotosData(post_id=post, name="Gorilla").save()
     return redirect(reverse("upload"))
 
 
