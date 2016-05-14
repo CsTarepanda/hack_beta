4b2396202617dc12d45ca3b4ab134276aab6730f
CsTarepanda <sankaku900622@yahoo.co.jp>
2016-05-15 05:37:20 +0900

    画像アップロード完成？name: <user><time>.<ext>

--- missphoto/postUpload.py
+++ missphoto/postUpload.py
@@ -4,6 +4,8 @@ from django.core.urlresolvers import reverse
 from django.core.files.storage import default_storage
 from django.core.files.base import ContentFile
 
+from time import time
+
 
 def index(request):
     if request.method == "POST":
@@ -14,7 +16,7 @@ def index(request):
 
 def post(request):
     for name, value in request.FILES.items():
-        default_storage.save("missphoto/static/photos/{}".format(value), ContentFile(value.read()))
+        default_storage.save("missphoto/static/photos/{}{}.{}".format(request.user, int(time()), str(value).split(".")[-1]), ContentFile(value.read()))
     return redirect(reverse("upload"))
 
 
