23007dd365c22e764ebc2836a78fd054f0b1f92d
CsTarepanda <sankaku900622@yahoo.co.jp>
2016-05-15 09:30:21 +0900

    Gorilla決め打ちを排除

--- missphoto/postUpload.py
+++ missphoto/postUpload.py
@@ -20,12 +20,12 @@ def index(request):
 
 def post(request):
     for name, value in request.FILES.items():
-        filename = "missphoto/static/photos/{}{}.{}".format(request.user, int(time()), str(value).split(".")[-1])
-        default_storage.save(filename, ContentFile(value.read()))
+        filename = "{}{}.{}".format(request.user, int(time()), str(value).split(".")[-1])
+        default_storage.save("missphoto/static/photos/" +filename, ContentFile(value.read()))
         post = Post(user_id=request.user)
         post.save()
         # - - - 無理やり cloud vision
-        subprocess.call("php missphoto/gcv.php {} > missphoto/gcv_result".format(filename), shell=True)
+        subprocess.call("php missphoto/gcv.php missphoto/static/photos/{} > missphoto/gcv_result".format(filename), shell=True)
         re = open("missphoto/gcv_result")
         re.readline()
         result = "{"
@@ -41,7 +41,7 @@ def post(request):
         print(result)
         for i in result:
             Tag(post_id=post, name=i).save()
-        PhotosData(post_id=post, name="Gorilla").save()
+        PhotosData(post_id=post, name=filename).save()
     return redirect(reverse("upload"))
 
 
