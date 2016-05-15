2786150d7675efe9b1b718d7603cab239f39ebb9
sinshutukibotu <c0114292a2@edu.teu.ac.jp>
2016-05-15 10:45:15 +0900

    リアクションのfix

--- missphoto/postReaction.py
+++ missphoto/postReaction.py
@@ -14,18 +14,22 @@ def index(request, reaction):
 
 
 def good_post(request, post_data):
-    if Good.objects.filter(user_id=request.user).exists():
-        return redirect(reverse("index"))
     post_id = post_data.get("post_id")
+    good = Good.objects.filter(user_id=request.user).filter(post_id=post_id)
+    if good.exists():
+        good[0].delete()
+        return redirect(reverse("index"))
     post = Post.objects.get(id=post_id)
     Good(user_id=request.user, post_id=post).save()
     return redirect(reverse("upload"))
 
 
 def bad_post(request, post_data):
-    if Bad.objects.filter(user_id=request.user).exists():
-        return redirect(reverse("index"))
     post_id = post_data.get("post_id")
+    bad = Bad.objects.filter(user_id=request.user)
+    if bad.exists():
+        bad[0].delete()
+        return redirect(reverse("index"))
     post = Post.objects.get(id=post_id)
     Bad(user_id=request.user, post_id=post).save()
     return redirect(reverse("upload"))
